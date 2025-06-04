afdfcbd962dd8d0398b06cc850395b835f053ed6
Lucas Franceschino <lucas@cryspen.com>
2025-06-04 15:18:37 +0200

    doc(rust-engine): misc

--- rust-printer/src/ast/derives.rs
+++ rust-printer/src/ast/derives.rs
@@ -1,3 +1,10 @@
+//! This module defines a few aliases for groups of derive clauses that are
+//! common and repeated a lot thoughtout the AST and its helper types.
+//!
+//! Instead of writting the same `#[derive(...)]` clause on top of each item of
+//! the AST, we just write `#[apply(derive_AST)]`, given `apply` and
+//! `derive_AST` are in scope.
+
 pub use macro_rules_attribute::{apply, attribute_alias};
 
 attribute_alias! {
