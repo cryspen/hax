415936aef76f24bb47235ab3dff001452cbb4521
Clément Blaudeau <clement.blaudeau@gmail.com>
2025-06-05 15:51:54 +0200

    wip

--- rust-engine/src/generic_printer/doc_printer.rs
+++ rust-engine/src/generic_printer/doc_printer.rs
@@ -1,20 +1,46 @@
-use super::{print_context::PrintContext, print_view::origin, to_print_view::ToPrintView};
+use super::{
+    print_context::{PrintContext, PrintContextPayload},
+    to_print_view::*,
+};
+
 use pretty::RcDoc;
 use std::any::Any;
+use std::rc::Rc;
 
-use crate::ast::Attribute;
+use super::{print_view, to_print_view};
+use crate::ast;
 
 pub use crate::generic_printer::print_view as destination;
 
-// A represent the type of annotations
-pub trait ToDoc<T, A = ()> {
-    fn to_doc(&self, x: T) -> RcDoc<A>;
+pub trait Printer {
+    fn resugar_expr(e: ast::Expr) -> Option<super::resugar::ResugaredExprKind>;
 }
 
-fn print<'a, T, P, A>(x: PrintContext<'a, T>, p: P) -> RcDoc<A>
+// A represent the type of annotations
+pub trait ToDoc<'a, T> {
+    fn to_doc(&self, x: T, p: PrintContextPayload<'a>) -> RcDoc<()>;
+}
+
+pub fn print<'a, T, P>(x: PrintContext<'a, T>, p: &'a P) -> RcDoc<'a, ()>
 where
     T: ToPrintView<'a>,
-    P: ToDoc<<T as ToPrintView<'a>>::Out, A>,
+    &'a T: Into<ast::node::NodeRef<'a>>,
+    P: ToDoc<'a, <T as ToPrintView<'a>>::Out>,
 {
-    p.to_doc(x.to_print_view(x.into()))
+    let payload = x.payload.clone();
+    p.to_doc(x.value.to_print_view(Some(Rc::new(x.into()))), payload)
 }
+
+// impl<'a, P: Printer + ToDoc<'a, print_view::Expr<'a>>> ToDoc<'a, ast::Expr> for P {
+//     fn to_doc(&self, x: ast::Expr, p: PrintContextPayload<'a>) -> RcDoc<()> {
+//         // TODO Apply resugaring
+//         let v: print_view::Expr = (x.to_print_view(Some(Rc::new(x.into()))));
+//         self.to_doc(
+//             v,
+//             PrintContextPayload {
+//                 position: "root".to_string(),
+//                 parent: None,
+//             },
+//         )
+//     }
+// }
--- rust-engine/src/generic_printer/lean.rs
+++ rust-engine/src/generic_printer/lean.rs
@@ -1,37 +1,77 @@
-use super::{
-    doc_printer::*, print_context::PrintContext, print_view::*, to_print_view::ToPrintAst,
-};
-use pretty::{BoxDoc, Doc};
+use crate::ast::Literal;
+
+use super::{doc_printer::*, print_context::*, print_view::*, to_print_view::*};
+use pretty::{BoxDoc, RcDoc};
 use std::any::Any;
+
+use crate::ast;
 
 pub struct LeanPrinter;
 
-impl ToDoc<LeanPrinter> for PrimitiveTy {
-    fn to_doc(&self, _printer: &LeanPrinter) -> Doc<'_, ()> {
-        match self {
+impl super::doc_printer::Printer for LeanPrinter {
+    fn resugar_expr(e: crate::ast::Expr) -> Option<super::resugar::ResugaredExprKind> {
+        None
+    }
+}
+
+impl<'a> ToDoc<'a, PrimitiveTy<'a>> for LeanPrinter {
+    fn to_doc(
+        &self,
+        x: PrimitiveTy<'a>,
+        p: super::print_context::PrintContextPayload<'a>,
+    ) -> RcDoc<()> {
+        match x {
             PrimitiveTy::Bool => todo!(),
             PrimitiveTy::Int(print_context) => todo!(),
+            PrimitiveTy::Float(print_context) => todo!(),
+            PrimitiveTy::Char => todo!(),
+            PrimitiveTy::Str => todo!(),
         }
     }
 }
-impl ToDoc<LeanPrinter> for ExprKind {
-    fn to_doc(&self, _printer: &LeanPrinter) -> Doc<'_, ()> {
-        match self {
+
+// impl<'a> ToDoc<'a, Box<T>>
+
+impl<'a> ToDoc<'a, Expr<'a>> for LeanPrinter {
+    //
+    // impl<'a, T: OpenPrintContext<'a>> OpenPrintContext<'a> for Box<T> {
+    //     type Inner = Box<T::Inner>;
+    //     fn open(ctx: PrintContext<'a, Self>) -> Self::Inner {
+    //         let value: &'a T = &*ctx.value;
+    //         Box::new(
+    //             PrintContext {
+    //                 value,
+    //                 payload: ctx.payload.clone(),
+    //             }
+    //             .open(),
+    //         )
+    //     }
+    // }
+
+    fn to_doc(&self, x: Expr<'a>, p: super::print_context::PrintContextPayload<'a>) -> RcDoc<()> {
+        let value: &ast::ExprKind = &*x.kind.value;
+        super::doc_printer::print(
+            PrintContext {
+                value: value,
+                payload: x.kind.payload,
+            },
+            self,
+        )
+    }
+}
+
+impl<'a> ToDoc<'a, ExprKind<'a>> for LeanPrinter {
+    fn to_doc(
+        &self,
+        x: ExprKind<'a>,
+        p: super::print_context::PrintContextPayload<'a>,
+    ) -> RcDoc<()> {
+        match x {
             ExprKind::If {
                 condition,
                 then,
                 else_,
-            } => match else_ {
-                Some(else_branch) => doc![
-                    "if ",
-                    condition.to_doc(p),
-                    " then ",
-                    then.to_doc(p),
-                    " else ",
-                    else_branch.to_doc(p)
-                ],
-                None => todo!(),
-            },
+            } => super::doc_printer::print(then, self),
             ExprKind::App {
                 head,
                 args,
@@ -51,14 +91,20 @@ impl ToDoc<LeanPrinter> for ExprKind {
             ExprKind::Match { scrutinee, arms } => todo!(),
             ExprKind::Tuple(print_context) => todo!(),
             ExprKind::Borrow { mutable, inner } => todo!(),
+            ExprKind::AddressOf { mutability, inner } => todo!(),
             ExprKind::Deref(print_context) => todo!(),
             ExprKind::Let { lhs, rhs, body } => todo!(),
             ExprKind::GlobalId(print_context) => todo!(),
             ExprKind::LocalId(print_context) => todo!(),
-            ExprKind::Error(print_context) => todo!(),
             ExprKind::Ascription { e, ty } => todo!(),
             ExprKind::Assign { lhs, value } => todo!(),
-            ExprKind::Loop { body, label } => todo!(),
+            ExprKind::Loop {
+                body,
+                kind,
+                state,
+                control_flow,
+                label,
+            } => todo!(),
             ExprKind::Break { value, label } => todo!(),
             ExprKind::Return { value } => todo!(),
             ExprKind::Continue { label } => todo!(),
@@ -67,6 +113,44 @@ impl ToDoc<LeanPrinter> for ExprKind {
                 body,
                 captures,
             } => todo!(),
+            ExprKind::Quote { contents } => todo!(),
+            ExprKind::Error(print_context) => todo!(),
         }
     }
+}
+
+impl<'a> ToDoc<'a, Literal> for LeanPrinter {
+    fn to_doc(&self, x: Literal, p: super::print_context::PrintContextPayload<'a>) -> RcDoc<()> {
+        match x {
+            Literal::String(symbol) => todo!(),
+            Literal::Char(_) => todo!(),
+            Literal::Bool(true) => RcDoc::text("true"),
+            Literal::Bool(false) => RcDoc::text("false"),
+            Literal::Int {
+                value,
+                negative,
+                kind,
+            } => todo!(),
+            Literal::Float {
+                value,
+                negative,
+                kind,
+            } => todo!(),
+        }
+    }
+}
+
+#[test]
+fn lit_test() {
+    let ast = Literal::Bool(true);
+    let doc = LeanPrinter.to_doc(
+        ast,
+        PrintContextPayload {
+            position: "root".into(),
+            parent: None,
+        },
+    );
+    let mut w = Vec::new();
+    doc.render(80, &mut w).unwrap();
+    println!("{}", String::from_utf8(w).unwrap());
 }
