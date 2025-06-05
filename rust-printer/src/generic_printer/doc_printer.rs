47f827de9f2176769a982687618fd142cd7a516a
Clément Blaudeau <clement.blaudeau@gmail.com>
2025-06-05 11:38:51 +0200

    Add pretty dependency

--- rust-printer/src/generic_printer/doc_printer.rs
+++ rust-printer/src/generic_printer/doc_printer.rs
@@ -1,5 +1,5 @@
 use super::{print_context::PrintContext, print_view::origin, to_print_view::ToPrintView};
-use pretty::{BoxDoc, Doc};
+use pretty::RcDoc;
 use std::any::Any;
 
 use crate::ast::Attribute;
@@ -8,13 +8,13 @@ pub use crate::generic_printer::print_view as destination;
 
 // A represent the type of annotations
 pub trait ToDoc<T, A = ()> {
-    fn to_doc(&self, x: T) -> Doc<'_, A>;
+    fn to_doc(&self, x: T) -> RcDoc<A>;
 }
 
-fn print<'a, T, P>(x: PrintContext<'a, T>, p: P) -> Doc<'_>
+fn print<'a, T, P, A>(x: PrintContext<'a, T>, p: P) -> RcDoc<A>
 where
     T: ToPrintView<'a>,
-    P: ToDoc<<T as ToPrintView<'a>>::Out>,
+    P: ToDoc<<T as ToPrintView<'a>>::Out, A>,
 {
     p.to_doc(x.to_print_view(x.into()))
 }
