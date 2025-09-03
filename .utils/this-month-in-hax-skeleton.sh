b24113ae1ffb3400ceb8f2ea2676fe396e19eeaf
Lucas Franceschino <lucas@cryspen.com>
2025-09-03 16:57:22 +0200

    wip

--- .utils/this-month-in-hax-skeleton.sh
+++ .utils/this-month-in-hax-skeleton.sh
@@ -5,6 +5,9 @@
 
 set -e
 
+# Go to the folder of blog posts
+cd $(git rev-parse --show-toplevel)/docs/blog/posts/this-month-in-hax
+
 # By default, use `cryspen/hax`, and the month and year from two weeks ago
 repo="--repo cryspen/hax"
 month=$(date -d "14 days ago" +'%m')
@@ -12,9 +15,6 @@ year=$(date -d "14 days ago" +'%Y')
 
 # Set date formatting to English
 export LC_ALL=C
-
-# Go to the folder of blog posts
-cd $(git rev-parse --show-toplevel)/docs/blog/posts/this-month-in-hax
 
 # Parse command line arguments.
 all_args=("$@")
@@ -30,18 +30,23 @@ done
 
 report() {
     # Calculate the first day of the month
-    start=$(date -d "$year-$month-01" '+%Y-%m-%d')
+    start=$(date -u -d "$year-$month-01" +"%Y-%m-%dT%H:%M:%SZ")
 
     # Get the next month
-    end=$(date -d "$year-$month-01 + 1 month - 1 day" '+%Y-%m-%d')
+    end=$(date -u -d "$year-$month-01 + 1 month - 1 day" +"%Y-%m-%dT%H:%M:%SZ")
 
     # Get all closed PRs with number, title, and description
-    pr_data=$(gh pr list $repo --state merged --limit 100 --search "merged:$start..$end" --json number,title,url,author)
+    pr_data=$(
+        gh pr list $repo --state merged --limit 1000 \
+            --json number,title,url,author,mergedAt \
+            --jq "map(select(.mergedAt >= \"$start\" and .mergedAt <= \"$end\")) | .[] | {number, title, url, author}" | jq -s
+    )
 
     echo "In $(date -d "$year-$month-01" +"%B"), we successfully merged **$(echo "$pr_data" | jq -r 'length')** pull requests**!"
     echo ""
     echo "<DESCRIPTION>"
     echo ""
+    echo "### Full list of PRs"
 
     # Extract markdown list with jq
     echo "$pr_data" | jq -r '.[] | . | "* \\#\(.number): [\(.title)](\(.url))"'
@@ -78,13 +83,15 @@ pick_author() {
 }
 
 author=$(pick_author)
+author=lucas
 
 BLOG_POST_FILE="$year-$month.md"
+
 
 cat << HEADER > $BLOG_POST_FILE
 ---
 authors:
-- $author
+  - $author
 title: "This Month in Hax: $(date -d "$year-$month-15" +"%B %Y")"
 date: $(date +"%Y-%m-%d")
 ---
@@ -92,14 +99,34 @@ date: $(date +"%Y-%m-%d")
 HEADER
 report >> $BLOG_POST_FILE
 
+BLOG_POST="$(cat $BLOG_POST_FILE)"
+
+# Go to root
+cd $(git rev-parse --show-toplevel)
+BRANCH="this-month-in-hax-blog-post-$year-$month"
+echo $BRANCH > this-month-in-hax-branch
+
 # Echo the author's handle
-cat <<MESSAGE
-This is an auto-generated skeleton for the "This Month in hax" blog series.
-It is an empty blog post with the list of PRs pushed in $(date -d "$year-$month-01" +"%B %Y") and a list of contributor.
+cat <<MESSAGE > this-month-in-hax-issue.yml
+---
+title: Write This Month in Hax
+assignees: $(handle_of $author)
+---
+
+This is an auto-generated issue for the "This Month in hax" blog series.
+
+Branch \`$BRANCH\` have been created with the following template:
+\`\`\`md
+$(echo "$BLOG_POST")
+\`\`\`
+
+It is an skeleton blog post with the list of PRs pushed in $(date -d "$year-$month-01" +"%B %Y") and a list of contributor.
 
 Suggested person to pick this draft PR: @$(handle_of $author)
 
-## Actions
+## Action Items
  - [ ] Write the blog article
- - [ ] Release a new version of hax (see \`PUBLISHING.md\`)
+ - [ ] Release a new version of hax
+    - [ ] Follow \`PUBLISHING.md\`
+    - [ ] Create Github release
 MESSAGE
