83ec39e1d20e33279772778891bd302bac880a78
Clément Blaudeau <clement.blaudeau@gmail.com>
2025-09-11 11:43:23 +0200

    more clippy

--- rust-engine/src/ast/identifiers/global_id.rs
+++ rust-engine/src/ast/identifiers/global_id.rs
@@ -206,10 +206,7 @@ impl GlobalId {
     pub fn is_precondition(&self) -> bool {
         match self {
             GlobalId::Concrete(concrete_id) | GlobalId::Projector(concrete_id) => {
-                match concrete_id.suffix {
-                    Some(ReservedSuffix::Pre) => true,
-                    _ => false,
-                }
+                matches!(concrete_id.suffix, Some(ReservedSuffix::Pre))
             }
         }
     }
@@ -219,10 +216,7 @@ impl GlobalId {
     pub fn is_postcondition(&self) -> bool {
         match self {
             GlobalId::Concrete(concrete_id) | GlobalId::Projector(concrete_id) => {
-                match concrete_id.suffix {
-                    Some(ReservedSuffix::Post) => true,
-                    _ => false,
-                }
+                matches!(concrete_id.suffix, Some(ReservedSuffix::Post))
             }
         }
     }
