50c9f326ee9b69496e1f7353ab1777cb9ec3febf
Lucas Franceschino <lucas@cryspen.com>
2025-10-08 11:29:33 +0200

    fix(rengine/lean): fix line indentation

--- rust-engine/src/backends/lean.rs
+++ rust-engine/src/backends/lean.rs
@@ -325,19 +325,20 @@ const _: () = {
             docs![
                 intersperse!(
                     "
-        -- Experimental lean backend for Hax
-        -- The Hax prelude library can be found in hax/proof-libs/lean
-        import Hax
-        import Std.Tactic.Do
-        import Std.Do.Triple
-        import Std.Tactic.Do.Syntax
-        open Std.Do
-        open Std.Tactic
+-- Experimental lean backend for Hax
+-- The Hax prelude library can be found in hax/proof-libs/lean
+import Hax
+import Std.Tactic.Do
+import Std.Do.Triple
+import Std.Tactic.Do.Syntax
+open Std.Do
+open Std.Tactic
 
-        set_option mvcgen.warning false
-        set_option linter.unusedVariables false
+set_option mvcgen.warning false
+set_option linter.unusedVariables false
 
-        "
+
+"
                     .lines(),
                     hardline!(),
                 ),
