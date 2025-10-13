bbffc3e167cd38c7acbf58426d58e5cbdcfc417c
Clément Blaudeau <clement.blaudeau@gmail.com>
2025-10-13 19:06:59 +0200

    feat(rust-engine): Add a phase to reject expressions outside of DSL

---
+++ rust-engine/src/phase/reject_not_do_lean_dsl.rs
@@ -0,0 +1,94 @@
+//! Rejection Phase for patterns unsupported by Lean's do-notation DSL
+//!
+//! This module defines the phase that rejects unsupported interleavings of expressions and
+//! statements, built as a visitor.
+
+use crate::ast::*;
+use crate::ast::{diagnostics::*, visitors::*};
+use crate::phase::Phase;
+
+/// Expressions are either do-statements or do-expressions. The former can be downgraded into the
+/// latter.
+#[derive(Clone, Copy, Debug)]
+enum DoDSLExprKind {
+    Statement,
+    Expression,
+}
+
+/// Gives the "kind" of an expression in the do-notation DSL
+fn dsl_expr_kind(expr_kind: &ExprKind) -> DoDSLExprKind {
+    match expr_kind {
+        ExprKind::If { .. } | ExprKind::Match { .. } | ExprKind::Let { .. } => {
+            DoDSLExprKind::Statement
+        }
+        _ => DoDSLExprKind::Expression,
+    }
+}
+
+/// The default value for entry points of expression (function items, function impl items)
+impl Default for DoDSLExprKind {
+    fn default() -> Self {
+        Self::Statement
+    }
+}
+
+/// Placeholder struct for the rejection phase
+#[derive(Default)]
+pub struct RejectNotDoLeanDSL;
+
+/// Visitor internal state
+#[setup_error_handling_struct]
+#[derive(Default)]
+pub struct RejectNotDoLeanDSLVisitor {
+    /// Expected kind for the visited expression. Used by `visit_expr`, ignored by other methods
+    dsl_expr_kind: DoDSLExprKind,
+}
+
+impl VisitorWithContext for RejectNotDoLeanDSLVisitor {
+    fn context(&self) -> Context {
+        Context::Phase(stringify!(RejectNotDoLeanDSL).to_string())
+    }
+}
+
+impl AstVisitorMut for RejectNotDoLeanDSLVisitor {
+    setup_error_handling_impl!();
+
+    fn visit_expr(&mut self, expr: &mut Expr) {
+        use DoDSLExprKind::*;
+        let previous_dsl_expr_kind = self.dsl_expr_kind;
+        self.dsl_expr_kind = match (self.dsl_expr_kind, dsl_expr_kind(&expr.kind)) {
+            // A do-expression cannot be upgraded to a do-statement, we throw an error
+            (Expression, Statement) => {
+                self.error(
+                    expr.clone(),
+                    DiagnosticInfoKind::ExplicitRejection {
+                        reason: "This interleaving of expression and statements does not fit in Lean's do-notation DSL.\
+                                 \nYou may try hoisting out let-bindings and control-flow.\
+                                 \nSee issue https://github.com/hacspec/hax/issues/1741"
+                            .to_string(),
+                    },
+                );
+                Statement
+            }
+            // Closures body are do-statement, as a `do` keyword is introduced
+            (_, _) if matches!(&*expr.kind, ExprKind::Closure { .. }) => Statement,
+            // In other cases, we keep the computed kind
+            (_, kind) => kind,
+        };
+        self.visit_inner(expr);
+        self.dsl_expr_kind = previous_dsl_expr_kind;
+    }
+
+    fn visit_ty(&mut self, ty: &mut Ty) {
+        if let TyKind::Array { length, .. } = ty.kind_mut() {
+            self.visit_inner(&mut *length);
+        }
+    }
+}
+
+impl Phase for RejectNotDoLeanDSL {
+    fn apply(&self, items: &mut Vec<Item>) {
+        // Entry points are statements
+        RejectNotDoLeanDSLVisitor::default().visit(items)
+    }
+}
