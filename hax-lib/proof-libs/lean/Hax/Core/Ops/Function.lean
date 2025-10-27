239a68ccb9237e5fff72ca0c9515a537a73d3eeb
Clément Blaudeau <clement.blaudeau@gmail.com>
2025-10-27 21:05:48 +0100

    test+fix(core-models/lean): core.ops.functions

--- hax-lib/proof-libs/lean/Hax/Core/Ops/Function.lean
+++ hax-lib/proof-libs/lean/Hax/Core/Ops/Function.lean
@@ -19,23 +19,23 @@ class Fn (Self Args : Type) {Output: Type} where
   [_constr_10219838627318688691 : (FnOnce Self Args (Output := Output))]
   call : Self -> Args -> Result Output
 
-instance {α β} : FnOnce (α → β) α (Output := β) where
+instance {α β} : FnOnce (α → Result β) α (Output := β) where
   Output := β
   call_once f x := f x
 
-instance {α β} : FnOnce (α → β) (Tuple1 α) (Output := β) where
+instance {α β} : FnOnce (α → Result β) (Tuple1 α) (Output := β) where
   Output := β
   call_once f x := f x._0
 
-instance {α β γ : Type} : FnOnce (α → β → γ) (Tuple2 α β) (Output := γ) where
+instance {α β γ : Type} : FnOnce (α → β → Result γ) (Tuple2 α β) (Output := γ) where
   Output := γ
   call_once f x := f x._0 x._1
 
-instance {α β} : Fn (α → β) α (Output := β) where
+instance {α β} : Fn (α → Result β) α (Output := β) where
   call f x := (FnOnce.call_once) f x
 
-instance {α β} : Fn (α → β) (Tuple1 α) (Output := β) where
+instance {α β} : Fn (α → Result β) (Tuple1 α) (Output := β) where
   call f x := (FnOnce.call_once) f x
 
-instance {α β γ} : Fn (α → β → γ) (Tuple2 α β) (Output := γ) where
+instance {α β γ} : Fn (α → β → Result γ) (Tuple2 α β) (Output := γ) where
   call f x := (FnOnce.call_once) f x
