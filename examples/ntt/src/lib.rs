e9508f5a2e8abd73932fda85adaf2a1055418414
karthikbhargavan <karthik.bhargavan@gmail.com>
2025-12-09 08:46:50 +0100

    looped up

--- examples/ntt/src/lib.rs
+++ examples/ntt/src/lib.rs
@@ -1,5 +1,4 @@
 const MONTGOMERY_SHIFT: u8 = 16;
-const MONTGOMERY_R: i32 = 1 << MONTGOMERY_SHIFT;
 const INVERSE_OF_MODULUS_MOD_MONTGOMERY_R: u32 = 62209; // FIELD_MODULUS^{-1} mod MONTGOMERY_R
 
 
@@ -12,7 +11,9 @@ mod spec {
     pub(crate) fn bounded_i16(x: i16, b: i16) -> bool {
         b >= 0 && x >= -b && x <= b
     }
-    pub(crate) fn bounded_i16_array(x: &[i16; 16], b: i16) -> bool {
+
+    // Can be replaced by an invocation to forall, but that turns it into a Prop
+    pub(crate) fn bounded_i16x16(x: &[i16; 16], b: i16) -> bool {
         bounded_i16(x[0], b)
             && bounded_i16(x[1], b)
             && bounded_i16(x[2], b)
@@ -31,25 +32,47 @@ mod spec {
             && bounded_i16(x[15], b)
     }
 
+    // Can be replaced by an invocation to forall, but that turns it into a Prop
+    pub(crate) fn bounded_i16x8(x: &[i16; 8], b: i16) -> bool {
+        bounded_i16(x[0], b)
+            && bounded_i16(x[1], b)
+            && bounded_i16(x[2], b)
+            && bounded_i16(x[3], b)
+            && bounded_i16(x[4], b)
+            && bounded_i16(x[5], b)
+            && bounded_i16(x[6], b)
+            && bounded_i16(x[7], b)
+    }
+
+    // Keeping this opaque speeds up proofs in F*
     pub(crate) fn modp(x: Int) -> Int {
         x.rem_euclid(FIELD_MODULUS.to_int())
     }
 
+    // A functional spec for montgomery reduction
     pub(crate) fn montgomery_reduce_i32_spec(value: i32, result: i16) -> bool {
         modp(result.to_int()) == 
         modp(value.to_int() * 169.to_int())
     }
 
-    pub(crate) fn ntt_mul_spec(a0: i16, a1: i16, b0: i16, b1: i16, zeta: i16, c0: i32, c1: i32) -> bool {
+    // A functional spec for even-index NTT multiplication
+    pub(crate) fn ntt_mul_spec_0(a0: i16, a1: i16, b0: i16, b1: i16, zeta: i16, c0: i32) -> bool {
         modp(c0.to_int()) == 
         modp(a0.to_int() * b0.to_int() + 
-             a1.to_int() * b1.to_int() * modp(zeta.to_int() * 169.to_int())) &&
+             a1.to_int() * b1.to_int() * modp(zeta.to_int() * 169.to_int()))
+    }
+
+    // A functional spec for odd-index NTT multiplication
+    pub(crate) fn ntt_mul_spec_1(a0: i16, a1: i16, b0: i16, b1: i16, c1: i32) -> bool {
         modp(c1.to_int()) == 
         modp(a0.to_int() * b1.to_int() + 
              a1.to_int() * b0.to_int()) 
     }
 }
 
+/// More precise post-condition:
+/// spec::montgomery_reduce_i32_spec(value, result)
+/// Requires proof annotations
 #[hax_lib::requires(spec::bounded_i32(value, 32768*3328))]
 #[hax_lib::ensures(|result| spec::bounded_i16(result, 3328))]
 fn montgomery_reduce_i32(value: i32) -> i16 {
@@ -61,33 +84,56 @@ fn montgomery_reduce_i32(value: i32) -> i16 {
     result
 }
 
-#[hax_lib::fstar::options("--z3rlimit 100")]
-#[hax_lib::requires(i < 8 && out.len() >= 16 && spec::bounded_i16(zeta, 1664) &&
-                    spec::bounded_i16_array(a,3328) && spec::bounded_i16_array(b,3328))]
+/// More precise post-condition:
+/// spec::ntt_mul_spec_0(a0, a1, b0, b1, zeta, result)
+/// Requires stronger post-condition on montgomery and proof annotations.
+#[hax_lib::requires(spec::bounded_i16(zeta, 1664) &&
+                    spec::bounded_i16(a0,3328) && spec::bounded_i16(a1,3328) &&
+                    spec::bounded_i16(b0,3328) && spec::bounded_i16(b1,3328))]
+#[hax_lib::ensures(|result| spec::bounded_i32(result, 3328*3328*2))]
+fn ntt_multiply_binomials_0(
+    a0: i16,
+    a1: i16,
+    b0: i16,
+    b1: i16,
+    zeta: i16,
+) -> i32 {
+    let a0_b0 = (a0 as i32) * (b0 as i32);
+    let b1_zeta = montgomery_reduce_i32((b1 as i32) * (zeta as i32));
+    let a1_b1_zeta = (a1 as i32) * (b1_zeta as i32);
+    a0_b0 + a1_b1_zeta
+}
+
+#[hax_lib::requires(spec::bounded_i16(a0,3328) && spec::bounded_i16(a1,3328) &&
+                    spec::bounded_i16(b0,3328) && spec::bounded_i16(b1,3328))]
+#[hax_lib::ensures(|result| spec::bounded_i32(result, 3328*3328*2) 
+                        && spec::ntt_mul_spec_1(a0, a1, b0, b1, result))]
+fn ntt_multiply_binomials_1(
+    a0: i16,
+    a1: i16,
+    b0: i16,
+    b1: i16,
+) -> i32 {
+    let a0_b1 = (a0 as i32) * (b1 as i32);
+    let a1_b0 = (a1 as i32) * (b0 as i32);
+    a0_b1 + a1_b0
+}
+
+/// We may want to turn the fixed-length arrays into slices, 
+/// but this will require additional preconditions and loop invariants
+#[hax_lib::requires(spec::bounded_i16x8(zetas, 1664) &&
+                    spec::bounded_i16x16(a,3328) && 
+                    spec::bounded_i16x16(b,3328))]
 pub fn accumulating_ntt_multiply_binomials(
     a: &[i16; 16],
     b: &[i16; 16],
-    zeta: i16,
-    i: usize,
-    out: &mut [i32],
+    zetas: &[i16; 8],
+    out: &mut [i32; 16],
 ) {
-    let ai = a[2 * i];
-    let bi = b[2 * i];
-    let aj = a[2 * i + 1];
-    let bj = b[2 * i + 1];
-
-    let ai_bi = (ai as i32) * (bi as i32);
-    let bj_zeta_ = (bj as i32) * (zeta as i32);
-    let bj_zeta = montgomery_reduce_i32(bj_zeta_);
-    let aj_bj_zeta = (aj as i32) * (bj_zeta as i32);
-    let ai_bi_aj_bj = ai_bi + aj_bj_zeta;
-    let o0 = ai_bi_aj_bj;
-
-    let ai_bj = (ai as i32) * (bj as i32);
-    let aj_bi = (aj as i32) * (bi as i32);
-    let ai_bj_aj_bi = ai_bj + aj_bi;
-    let o1 = ai_bj_aj_bi;
-
-    out[2 * i] = out[2 * i].wrapping_add(o0);
-    out[2 * i + 1] = out[2 * i + 1].wrapping_add(o1);
+    for i in 0..8 {
+        let o0 = ntt_multiply_binomials_0(a[2*i], a[2*i+1], b[2*i], b[2*i+1], zetas[i]);
+        let o1 = ntt_multiply_binomials_1(a[2*i], a[2*i+1], b[2*i], b[2*i+1]);
+        out[2*i] = out[2*i].wrapping_add(o0);
+        out[2*i+1] = out[2*i+1].wrapping_add(o1);
+    }
 }
