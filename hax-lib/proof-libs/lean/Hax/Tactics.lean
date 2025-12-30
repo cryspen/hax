683d1c74a16d3ae796a66d2f13248afec25a6022
Alexander Bentkamp <alexander@cryspen.com>
2025-12-30 14:51:01 +0100

    working prototype

--- hax-lib/proof-libs/lean/Hax/Tactics.lean
+++ hax-lib/proof-libs/lean/Hax/Tactics.lean
@@ -22,25 +22,26 @@ elab "hax_purify" : tactic =>
     let goal ← Lean.Elab.Tactic.getMainGoal
     let goalDecl ← goal.getDecl
     have goalType : Q(Type) := goalDecl.type
-    let some lastGoalFVarId := goalDecl.lctx.getFVarIds.back?
-      | Lean.Meta.throwTacticEx `hax_purify goal (m!"unexpected empty local context")
 
     let p ←
       match goalType with
       | ~q(@Subtype.{0} _ $p) => pure p
       | ~q(@Subtype.{1} _ $p) => pure p
       | _ => Lean.Meta.throwTacticEx `hax_purify goal (m!"unable to run hax_purify on this goal: {goalType}")
-    let lctx ← getLCtx
-    let localInsts ← getLocalInstances
-    let (_, _, p1) ← lambdaMetaTelescope p
+    let p1 := p
+    let (_, _, p) ← lambdaMetaTelescope p
+    let (_, _, p) ← if p.isForall then forallMetaBoundedTelescope p 1 else pure (default, default, p)
     -- let #[v] := v
     --   | Lean.Meta.throwTacticEx `hax_purify goal (m!"expected exactly one lambda-variable: {v}")
-    forallTelescope p1 fun vars (p2 : Q(Prop)) => do
-      match p2 with
-      | ~q(@Std.Do.Triple RustM _ _ Prop $condM _ _) => do --withLCtx lctx localInsts do
+    forallTelescope p fun vars (p : Q(Prop)) => do
+      match p with
+      | ~q(@Std.Do.Triple RustM _ _ Prop $condM _ _) => do
 
-        if vars.size != 0 && vars.size != 2 then
-          Lean.Meta.throwTacticEx `hax_purify goal (m!"expected either 0 or 2 forall-variables: {vars}")
+        let some lastGoalFVarId := (← getLCtx).getFVarIds.back?
+          | Lean.Meta.throwTacticEx `hax_purify goal (m!"unexpected empty local context")
+
+        if vars.size != 0 && vars.size != 1 then
+          Lean.Meta.throwTacticEx `hax_purify goal (m!"expected either 0 or 1 forall-variables: {vars}")
         logInfo m!"{p}"
         -- let p ← if vars.size == 2 then mkLambdaFVars #[v] p else pure p
         logInfo m!"{p}"
@@ -52,8 +53,13 @@ elab "hax_purify" : tactic =>
           let (_, x) ← vc.revertAfter lastGoalFVarId
           pure (← x.getDecl).type
         let wpc : Q(Prop) := foldAnd wpcs
-        let xs ← goal.applyN (← mkAppOptM `Subtype.mk #[none, p, wpc]) 1
+        logInfo vars
+        logInfo p1
+        let wpc ← if vars.size == 1 then mkLambdaFVars vars wpc else pure wpc
+        logInfo p1
+        let xs ← goal.applyN (← mkAppOptM `Subtype.mk #[none, p1, wpc]) 1
         replaceMainGoal xs
+        evalTactic (← `(tactic| intros))
 
       | _ => Lean.Meta.throwTacticEx `hax_purify goal (m!"expected triple, got: {p}")
 
@@ -63,7 +69,7 @@ elab "hax_purify" : tactic =>
 def Playground.square.spec (x : u8)  :
     Spec
       (requires := (if x > 10 then pure (x == 15) else pure (x == 2)))
-      (ensures := fun res => (pure (x == res)))
+      (ensures := fun res => (if x > 10 then pure (x == res) else pure (x == 0)))
       (do pure (x : u8) : RustM _) := {
   pureRequires := by hax_purify <;> mvcgen <;> grind
   pureEnsures := by hax_purify <;> mvcgen <;> grind
