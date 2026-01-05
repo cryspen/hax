08544c6b57d3e6d1d701b820b4c7fc0c9659bd1f
Alexander Bentkamp <alexander@cryspen.com>
2026-01-05 08:56:38 +0100

    feat: comparison specs for all int types

--- hax-lib/proof-libs/lean/Hax/Lib.lean
+++ hax-lib/proof-libs/lean/Hax/Lib.lean
@@ -258,38 +258,103 @@ infixl:60 " &&&? " => fun a b => pure (HAnd.hAnd a b)
   are also provided -/
 namespace Rust_primitives.Hax.Machine_int
 
-@[simp, spec, hax_bv_decide]
+@[simp, hax_bv_decide]
 def eq {α} (x y: α) [BEq α] : RustM Bool := pure (x == y)
-@[simp, spec, hax_bv_decide]
+
+@[simp, hax_bv_decide]
 def ne {α} (x y: α) [BEq α] : RustM Bool := pure (x != y)
-@[simp, hax_bv_decide]
-def lt {α} (x y: α) [(LT α)] [Decidable (x < y)] : RustM Bool :=
-  pure (x < y)
-@[simp, hax_bv_decide]
-def le {α} (x y: α) [(LE α)] [Decidable (x ≤ y)] : RustM Bool :=
-  pure (x ≤ y)
-@[simp, hax_bv_decide]
-def gt {α} (x y: α) [(LT α)] [Decidable (x > y)] : RustM Bool :=
-  pure (x > y)
-@[simp, hax_bv_decide]
-def ge {α} (x y: α) [(LE α)] [Decidable (x ≥ y)] : RustM Bool :=
-  pure (x ≥ y)
 
-@[spec]
-def lt_spec (x y : usize) : ⦃ ⌜ True ⌝ ⦄ lt x y ⦃ ⇓ r => ⌜ r = (x.toNat < y.toNat) ⌝ ⦄ := by
-  mvcgen [lt]; simp [USize64.lt_iff_toNat_lt]
+@[simp, hax_bv_decide]
+def lt {α} (x y: α) [(LT α)] [Decidable (x < y)] : RustM Bool := pure (x < y)
 
-@[spec]
-def le_spec (x y : usize) : ⦃ ⌜ True ⌝ ⦄ le x y ⦃ ⇓ r => ⌜ r = (x.toNat ≤ y.toNat) ⌝ ⦄ := by
-  mvcgen [lt]; simp [USize64.le_iff_toNat_le]
+@[simp, hax_bv_decide]
+def le {α} (x y: α) [(LE α)] [Decidable (x ≤ y)] : RustM Bool := pure (x ≤ y)
 
-@[spec]
-def gt_spec (x y : usize) : ⦃ ⌜ True ⌝ ⦄ gt x y ⦃ ⇓ r => ⌜ r = (x.toNat > y.toNat ) ⌝ ⦄ := by
-  mvcgen [lt]; simp [USize64.lt_iff_toNat_lt]
+@[simp, hax_bv_decide]
+def gt {α} (x y: α) [(LT α)] [Decidable (x > y)] : RustM Bool := pure (x > y)
 
-@[spec]
-def ge_spec (x y : usize) : ⦃ ⌜ True ⌝ ⦄ ge x y ⦃ ⇓ r => ⌜ r = (x.toNat ≥ y.toNat) ⌝ ⦄ := by
-  mvcgen [lt]; simp [USize64.le_iff_toNat_le]
+@[simp, hax_bv_decide]
+def ge {α} (x y: α) [(LE α)] [Decidable (x ≥ y)] : RustM Bool := pure (x ≥ y)
+
+open Lean in
+set_option hygiene false in
+macro "declare_comparison_specs" s:(&"signed" <|> &"unsigned") typeName:ident width:term : command => do
+
+  let signed ← match s.raw[0].getKind with
+  | `signed => pure true
+  | `unsigned => pure false
+  | _ => throw .unsupportedSyntax
+
+  if signed then
+    return ← `(
+      namespace $typeName
+
+      @[spec]
+      def eq_spec (x y : $typeName) : ⦃ ⌜ True ⌝ ⦄ eq x y ⦃ ⇓ r => ⌜ r = (x.toInt == y.toInt) ⌝ ⦄ := by
+        mvcgen [eq]; rw [← @Bool.coe_iff_coe]; simp [x.toInt_inj]
+
+      @[spec]
+      def ne_spec (x y : $typeName) : ⦃ ⌜ True ⌝ ⦄ ne x y ⦃ ⇓ r => ⌜ r = (x.toInt != y.toInt) ⌝ ⦄ := by
+        mvcgen [ne]; rw [← @Bool.coe_iff_coe]; simp [x.toInt_inj]
+
+      @[spec]
+      def lt_spec (x y : $typeName) : ⦃ ⌜ True ⌝ ⦄ lt x y ⦃ ⇓ r => ⌜ r = (x.toInt < y.toInt) ⌝ ⦄ := by
+        mvcgen [lt]; simp [x.lt_iff_toInt_lt]
+
+      @[spec]
+      def le_spec (x y : $typeName) : ⦃ ⌜ True ⌝ ⦄ le x y ⦃ ⇓ r => ⌜ r = (x.toInt ≤ y.toInt) ⌝ ⦄ := by
+        mvcgen [le]; simp [x.le_iff_toInt_le]
+
+      @[spec]
+      def gt_spec (x y : $typeName) : ⦃ ⌜ True ⌝ ⦄ gt x y ⦃ ⇓ r => ⌜ r = (x.toInt > y.toInt ) ⌝ ⦄ := by
+        mvcgen [gt]; simp [y.lt_iff_toInt_lt]
+
+      @[spec]
+      def ge_spec (x y : $typeName) : ⦃ ⌜ True ⌝ ⦄ ge x y ⦃ ⇓ r => ⌜ r = (x.toInt ≥ y.toInt) ⌝ ⦄ := by
+        mvcgen [ge]; simp [y.le_iff_toInt_le]
+
+      end $typeName
+    )
+  else return ← `(
+      namespace $typeName
+
+      @[spec]
+      def eq_spec (x y : $typeName) : ⦃ ⌜ True ⌝ ⦄ eq x y ⦃ ⇓ r => ⌜ r = (x.toNat == y.toNat) ⌝ ⦄ := by
+        mvcgen [eq]; rw [← @Bool.coe_iff_coe]; simp [x.toNat_inj]
+
+      @[spec]
+      def ne_spec (x y : $typeName) : ⦃ ⌜ True ⌝ ⦄ ne x y ⦃ ⇓ r => ⌜ r = (x.toNat != y.toNat) ⌝ ⦄ := by
+        mvcgen [ne]; rw [← @Bool.coe_iff_coe]; simp [x.toNat_inj]
+
+      @[spec]
+      def lt_spec (x y : $typeName) : ⦃ ⌜ True ⌝ ⦄ lt x y ⦃ ⇓ r => ⌜ r = (x.toNat < y.toNat) ⌝ ⦄ := by
+        mvcgen [lt]; simp [x.lt_iff_toNat_lt]
+
+      @[spec]
+      def le_spec (x y : $typeName) : ⦃ ⌜ True ⌝ ⦄ le x y ⦃ ⇓ r => ⌜ r = (x.toNat ≤ y.toNat) ⌝ ⦄ := by
+        mvcgen [le]; simp [x.le_iff_toNat_le]
+
+      @[spec]
+      def gt_spec (x y : $typeName) : ⦃ ⌜ True ⌝ ⦄ gt x y ⦃ ⇓ r => ⌜ r = (x.toNat > y.toNat ) ⌝ ⦄ := by
+        mvcgen [gt]; simp [y.lt_iff_toNat_lt]
+
+      @[spec]
+      def ge_spec (x y : $typeName) : ⦃ ⌜ True ⌝ ⦄ ge x y ⦃ ⇓ r => ⌜ r = (x.toNat ≥ y.toNat) ⌝ ⦄ := by
+        mvcgen [ge]; simp [y.le_iff_toNat_le]
+
+      end $typeName
+  )
+
+declare_comparison_specs signed Int8 8
+declare_comparison_specs signed Int16 16
+declare_comparison_specs signed Int32 32
+declare_comparison_specs signed Int64 64
+declare_comparison_specs signed ISize System.Platform.numBits
+declare_comparison_specs unsigned UInt8 8
+declare_comparison_specs unsigned UInt16 16
+declare_comparison_specs unsigned UInt32 32
+declare_comparison_specs unsigned UInt64 64
+declare_comparison_specs unsigned USize64 64
 
 end Rust_primitives.Hax.Machine_int
 
