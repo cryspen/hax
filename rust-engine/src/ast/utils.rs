384dc87f5bda2a1c8904237a03e3f0d33bd89649
Lucas Franceschino <lucas@cryspen.com>
2026-01-12 12:27:53 +0100

    feat(rengine/ast): add helpers to destruct `self`

--- rust-engine/src/ast/utils.rs
+++ rust-engine/src/ast/utils.rs
@@ -38,3 +38,33 @@ impl Metadata {
         crate::attributes::hax_attributes(&self.attributes)
     }
 }
+
+impl Pat {
+    /// Expects the pattern to be a simple binding `self`.
+    pub fn expect_self(&self) -> Option<LocalId> {
+        if let PatKind::Binding { var, .. } = self.kind()
+            && var.is_self()
+        {
+            Some(var.clone())
+        } else {
+            None
+        }
+    }
+}
+
+impl Item {
+    /// Returns a `LocalId` named `self` if the item is a standalone function
+    /// whose first argument is the keyword `self`. In other words, this
+    /// function returns a local identifier only for associated methods from
+    /// inherent `impl` blocks.
+    pub fn self_id(&self) -> Option<LocalId> {
+        if let ItemKind::Fn { params, .. } = self.kind()
+            && let [first, ..] = &params[..]
+            && let Some(self_id) = first.pat.expect_self()
+        {
+            Some(self_id.clone())
+        } else {
+            None
+        }
+    }
+}
