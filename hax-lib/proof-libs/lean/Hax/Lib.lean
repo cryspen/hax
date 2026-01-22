6f18703a2a700af1f014860f808a47e58275e2c9
Natasha <webmathew@gmail.com>
2026-01-22 10:07:42 +0300

    lean: add Core_models.Slice.Impl.is_empty

--- hax-lib/proof-libs/lean/Hax/Lib.lean
+++ hax-lib/proof-libs/lean/Hax/Lib.lean
@@ -860,6 +860,10 @@ def Rust_primitives.unsize {α n} (a: Vector α n) : RustM (Array α) :=
 @[simp, spec]
 def Core_models.Slice.Impl.len α (a: Array α) : RustM usize := pure a.size
 
+def Core_models.Slice.Impl.is_empty α (a : Array α) : RustM Bool := do
+  let n ← Core_models.Slice.Impl.len α a
+  pure (n == 0)
+
 /-
 
 # Vectors
