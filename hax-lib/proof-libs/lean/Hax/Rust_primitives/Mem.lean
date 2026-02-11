c96ed851955f45e2b487515382b3d5e16a298428
Alexander Bentkamp <alexander@cryspen.com>
2026-02-11 13:01:51 +0100

    feat(lean): add rust_primitives.mem.copy

--- hax-lib/proof-libs/lean/Hax/Rust_primitives/Mem.lean
+++ hax-lib/proof-libs/lean/Hax/Rust_primitives/Mem.lean
@@ -3,3 +3,5 @@ import Hax.Rust_primitives.Tuple
 
 def rust_primitives.mem.replace (α : Type) (dst : α) (src : α) :
   RustM (rust_primitives.hax.Tuple2 α α) := pure ⟨src, dst⟩
+
+def rust_primitives.mem.copy (α : Type) (x : α) : RustM α := pure x
