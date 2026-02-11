71d83cd8a89cf3de0fd1aab7bb20052bbc652ebd
Alexander Bentkamp <alexander@cryspen.com>
2026-02-11 14:28:49 +0100

    feat: communicate specs to mvcgen
    Fixes #1818

--- hax-lib/proof-libs/lean/Hax/MissingCore.lean
+++ hax-lib/proof-libs/lean/Hax/MissingCore.lean
@@ -1,7 +1,6 @@
 -- This module contains spurious dependencies of Core_models on Core, and
 -- should eventually be removed.
 
-import Hax.Rust_primitives
 import Hax.MissingCore.Clone
 import Hax.MissingCore.Marker
 import Hax.MissingCore.Num
--- tests/lean-tests/src/specs.rs
+++ tests/lean-tests/src/specs.rs
@@ -6,6 +6,12 @@ fn test(x: u8) -> u8 {
 
 #[hax_lib::requires(x > 0)]
 #[hax_lib::ensures(|r| r == x)]
+fn use_previous_result(x: u8) -> u8 {
+    test(x)
+}
+
+#[hax_lib::requires(x > 0)]
+#[hax_lib::ensures(|r| r == x)]
 #[hax_lib::lean::proof("by unfold lean_tests.specs.test_proof; hax_bv_decide")]
 fn test_proof(x: u8) -> u8 {
     x
