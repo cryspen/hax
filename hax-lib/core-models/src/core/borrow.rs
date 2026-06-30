eff3c6ec848679541e3b0990a7206f4748e9a6e9
Alexander Bentkamp <alexander@cryspen.com>
2026-06-30 11:58:04 +0200

    chore: remove old core-models

--- hax-lib/core-models/src/core/borrow.rs
+++
@@ -1,5 +0,0 @@
-/// See [`std::borrow::Borrow`]
-trait Borrow<Borrowed> {
-    /// See [`std::borrow::Borrow::borrow`]
-    fn borrow(&self) -> Borrowed;
-}
--- hax-lib/core-models/src/core/error.rs
+++
@@ -1,4 +0,0 @@
-use super::fmt::{Debug, Display};
-
-/// See [`std::error::Error`]
-pub trait Error: Display + Debug {}
