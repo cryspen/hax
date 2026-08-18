b2e95f3c26457287c56b2d01d70278f7f02283da
Alexander Bentkamp <alexander@cryspen.com>
2026-08-18 16:46:39 +0200

    docs: add file docs to Lean files

--- hax-lib/proof-libs/lean/CoreModels/HaxLib.lean
+++ hax-lib/proof-libs/lean/CoreModels/HaxLib.lean
@@ -1,2 +1,12 @@
 import CoreModels.HaxLib.Prop
 import CoreModels.HaxLib.Int
+import CoreModels.HaxLib.Funs
+
+/-!
+
+# Hax Lib
+
+This module contains Lean definitions for the `hax_lib` crate, in particular definitions
+that allow us to use mathematical integers in Rust specifications.
+
+-/
