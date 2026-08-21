1f410e22d4ccf12d54cc05bbd1588ed8cd9d7e20
Maxime Buyse <maxime@cryspen.com>
2026-08-21 08:48:23 +0200

    chore(core-models): re-extract for the lean backend route

--- hax-lib/proof-libs/lean/CoreModels/Alloc/Types.lean
+++ hax-lib/proof-libs/lean/CoreModels/Alloc/Types.lean
@@ -49,11 +49,6 @@ structure borrow.ToOwned (Self : Type) where
 @[reducible]
 def boxed.Box (T : Type) := T
 
-/-- [alloc::collections::btree::set::BTreeSet]
-    Source: 'src/lib.rs', lines 185:12-185:56 -/
-def collections.btree.set.BTreeSet (T : Type) (U : Type) :=
-  core.option.Option T × core.option.Option U
-
 /-- [alloc::collections::vec_deque::VecDeque]
     Source: 'src/lib.rs', lines 209:8-209:75
     Visibility: public -/
@@ -67,24 +62,24 @@ def collections.vec_deque.into_iter.IntoIter (T : Type) (A : Type) :=
   rust_primitives.sequence.Seq T × core.marker.PhantomData A
 
 /-- [alloc::slice::Dummy]
-    Source: 'src/lib.rs', lines 364:4-364:23 -/
+    Source: 'src/lib.rs', lines 366:4-366:23 -/
 @[reducible]
 def slice.Dummy (T : Type) := T
 
 /-- [alloc::vec::Vec]
-    Source: 'src/lib.rs', lines 515:4-515:34
+    Source: 'src/lib.rs', lines 517:4-517:34
     Visibility: public -/
 @[reducible]
 def vec.Vec (T : Type) := rust_primitives.sequence.Seq T
 
 /-- [alloc::vec::into_iter::IntoIter]
-    Source: 'src/lib.rs', lines 562:8-562:43
+    Source: 'src/lib.rs', lines 564:8-564:43
     Visibility: public -/
 @[reducible]
 def vec.into_iter.IntoIter (T : Type) := rust_primitives.sequence.Seq T
 
 /-- [alloc::vec::drain::Drain]
-    Source: 'src/lib.rs', lines 678:8-678:76
+    Source: 'src/lib.rs', lines 680:8-680:76
     Visibility: public -/
 def vec.drain.Drain (T : Type) (A : Type) :=
   rust_primitives.sequence.Seq T × core.marker.PhantomData A
